f569dc6c22d67eb33f877af4cca5c62639fcc87f
tomp736 <12602834+tomp736@users.noreply.github.com>
2023-02-21 20:52:29 +0100

    adding dns

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,7 +110,7 @@ data "hetznerdns_zone" "dns_zone" {
   name = "labrats.work"
 }
 
-resource "hetznerdns_record" "dns_record" {
+resource "hetznerdns_record_bastion" "dns_record" {
   for_each = module.bastion_node_group.nodes
 
   zone_id = data.hetznerdns_zone.dns_zone.id
@@ -120,7 +120,7 @@ resource "hetznerdns_record" "dns_record" {
   ttl     = 60
 }
 
-resource "hetznerdns_record" "dns_record" {
+resource "hetznerdns_record_haproxy" "dns_record" {
   for_each = module.haproxy_node_group.nodes
 
   zone_id = data.hetznerdns_zone.dns_zone.id
