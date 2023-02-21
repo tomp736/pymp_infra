82add03f47cf14760c68ad6d63f7b5c57b1aa0f7
tomp736 <12602834+tomp736@users.noreply.github.com>
2023-02-21 20:41:13 +0100

    adding dns

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,9 +6,17 @@ terraform {
     hcloud = {
       source = "hetznercloud/hcloud"
     }
+    hetznerdns = {
+      source  = "timohirt/hetznerdns"
+      version = "2.2.0"
+    }
   }
 }
 
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+provider "hetznerdns" {
+  apitoken = var.hcloud_token_dns
 }
